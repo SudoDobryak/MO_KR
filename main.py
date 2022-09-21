9965af4937dae94b5a189b77d8b7d759d3d30486
EldarKlyuev <e.s.klyuev@gmail.com>
2022-09-21 22:48:52 +0300

    It's working

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class DlgMain(QtWidgets.QMainWindow, gui.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(lambda xl_path=book, sheet_name=sheet: self.loadEcxelData(xl_path, sheet_name))
+        self.pushButton.clicked.connect(lambda _, xl_path=book, sheet_name=sheet: self.loadEcxelData(xl_path, sheet_name))
         self.pushButton_2.clicked.connect(self.alg)
 
     def loadEcxelData(self, excel_file_dir, worksheet_name):
@@ -43,17 +43,23 @@ class DlgMain(QtWidgets.QMainWindow, gui.Ui_MainWindow):
     def alg(self):
         m = symplex.gen_matrix(7, 2)
         symplex.constrain(m, f"0,2,5,3,0,2,4,G,470")
-        symplex.constrain(m, f"3,2,0,1,2,1,0,G,{self.textEdit.toPlainText()}")
+        symplex.constrain(m, f"3,2,0,1,2,1,0,G,450")
         symplex.obj(m, '10,25,0,35,30,20,10')
         a = symplex.minz(m)
-        self.textEdit_2.setText(f"X1 = {a['x1']}\n"
-                                f"X2 = {a['x2']}\n"
-                                f"X3 = {a['x3']}\n"
-                                f"X4 = {a['x4']}\n"
-                                f"X5 = {a['x5']}\n"
-                                f"X6 = {a['x6']}\n"
-                                f"X7 = {a['x7']}\n"
-                                f"Минимальная функция равна: {a['min']}")
+        m2 = symplex.gen_matrix(7, 2)
+        symplex.constrain(m2, f"0,2,5,3,0,2,4,G,470")
+        symplex.constrain(m2, f"3,2,0,1,2,1,0,G,{self.textEdit.toPlainText()}")
+        symplex.obj(m2, '10,25,0,35,30,20,10')
+        a2 = symplex.minz(m2)
+        self.textEdit_2.setText(f"X1 = {a['x1']}" + f"\t\tY1 = {a2['x1']}\n"
+                                f"X2 = {a['x2']}" + f"\t\tY2 = {a2['x2']}\n"
+                                f"X3 = {a['x3']}" + f"\t\tY3 = {a2['x3']}\n"
+                                f"X4 = {a['x4']}" + f"\t\tY4 = {a2['x4']}\n"
+                                f"X5 = {a['x5']}" + f"\t\tY5 = {a2['x5']}\n"
+                                f"X6 = {a['x6']}" + f"\t\tY6 = {a2['x6']}\n"
+                                f"X7 = {a['x7']}" + f"\t\tY7 = {a2['x7']}\n"
+                                f"Минимальная функция для 450 равна: {a['min']}\n"
+                                f"Минимальная функция для {self.textEdit.toPlainText()} равна: {a2['min']}")
 
 
 def main():
